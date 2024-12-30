951eaa2349fcd660fab73103cf8b868be9c6ad1b
Drombeys <dima.zolotov02@gmail.com>
2024-12-30 20:32:27 +0300

    Fix build

--- sdk/include/luabind/luabind_memory_allocator_inline.h
+++ sdk/include/luabind/luabind_memory_allocator_inline.h
@@ -44,7 +44,7 @@ typename luabind::memory_allocator<T>::const_pointer luabind::memory_allocator<T
 }
 
 template <typename T>
-typename luabind::memory_allocator::pointer luabind::memory_allocator::allocate			(size_type const n, void const* const p) const
+typename luabind::memory_allocator<T>::pointer luabind::memory_allocator<T>::allocate			(size_type const n, void const* const p) const
 {
 	pointer			__rslt = (pointer)call_allocator(p,n*sizeof(T));
 	if (!n)
