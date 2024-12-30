1a3c7a94ee72bd26e838995789e72aa2cccfb7dd
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2024-12-30 20:30:41 +0300

    добавлен dxStatGraphRender

--- src/Layers/xrRender/dxRenderFactory.cpp
+++ src/Layers/xrRender/dxRenderFactory.cpp
@@ -1,8 +1,7 @@
 #include "stdafx.h"
 #include "dxRenderFactory.h"
-//#include "dxStatGraphRender.h"
+#include "dxStatGraphRender.h"
 #include "dxLensFlareRender.h"
-//#include "dxConsoleRender.h"
 #include "dxThunderboltRender.h"
 #include "dxThunderboltDescRender.h"
 #include "dxRainRender.h"
@@ -31,8 +30,7 @@ dxRenderFactory RenderFactoryImpl;
 
 	RENDER_FACTORY_IMPLEMENT(UISequenceVideoItem)
 	RENDER_FACTORY_IMPLEMENT(UIShader)
-//	RENDER_FACTORY_IMPLEMENT(StatGraphRender)
-//	RENDER_FACTORY_IMPLEMENT(ConsoleRender)
+	RENDER_FACTORY_IMPLEMENT(StatGraphRender)
 	RENDER_FACTORY_IMPLEMENT(RenderDeviceRender)
 #	ifdef DEBUG
 //		RENDER_FACTORY_IMPLEMENT(ObjectSpaceRender)
