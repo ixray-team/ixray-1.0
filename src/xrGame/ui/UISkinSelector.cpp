958dd3ec7e66882469941dd6f810c6bb38f3497c
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2024-12-30 20:30:42 +0300

    Фиксы 16x9

--- src/xrGame/ui/UISkinSelector.cpp
+++ src/xrGame/ui/UISkinSelector.cpp
@@ -88,7 +88,8 @@ void CUISkinSelectorWnd::UpdateSkins(){
             m_pImage[i]->InitTextureEx(m_skins[i + m_firstSkin].c_str(), *m_shader);
 		else
 			m_pImage[i]->InitTexture(m_skins[i + m_firstSkin].c_str());
-		m_pImage[i]->RescaleRelative2Rect(m_pImage[i]->GetStaticItem()->GetOriginalRect());
+		// St4lker0k765: is it even necessary? (especially in non 4x3 case)
+		//m_pImage[i]->RescaleRelative2Rect(m_pImage[i]->GetStaticItem()->GetOriginalRect());
 
 		if (m_iActiveIndex - m_firstSkin == i)
 			m_pImage[i]->SetSelectedState(true);
